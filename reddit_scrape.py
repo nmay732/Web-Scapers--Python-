2f00c726702996d78e912006bf563c5e05f90a58
nmay732 <nmay@student.umass.edu>
2012-03-04 14:13:35 -0500

    accepts command line params; prints to outfile;

--- reddit_scrape.py
+++ reddit_scrape.py
@@ -1,17 +1,44 @@
+"""
+Notes:
+-Orders submissions by most upvotes NOT chronological
+-Comments include "[deleted]" for deleted comments
+-Subreddit input isn't case sensitive
+-Set limit to "None" for no limit
+-Reddit API limits to two requests / sec
+-Currently overwites outfiles if it already exists
+"""
+
+import sys
+import os
 import reddit
+
+usage = "python reddit_scrape.py subreddit limit outfile"
+
+if len(sys.argv) < 4:
+    print "\nABORTED: Too few args"
+    print usage
+    sys.exit();
+
+if sys.argv[2] != 'None':
+    limit_in = int(sys.argv[2])
+else:
+    limit_in = sys.argv[2]
 
 r = reddit.Reddit(user_agent='*')
 
-"""not case sensitive"""
-print "choose a subreddit:"
-subreddit = raw_input()
+submissions = r.get_subreddit(sys.argv[1]).get_top(limit=limit_in)
 
-submissions = r.get_subreddit(subreddit).get_top(limit=None)
+if not os.path.isfile(sys.argv[3]):
+    open(sys.argv[3], 'w').close()
 
-print "Comment Scraping..."
-for submission in submissions:
-    print "--SUBMISSION--"
-    print submission
-    print "--COMMENTS--"
-    for comment in submission.comments:
-            print comment
+with open(sys.argv[3], 'w') as f:
+    print "Submissions scraped:"
+    count = 0
+    for submission in submissions:
+        count += 1
+        print count
+        f.write("--SUBMISSION--\n")
+        f.write(str(submission) + "\n")
+        f.write("--COMMENTS--\n")
+        for comment in submission.comments:
+            f.write(str(comment) + "\n")
